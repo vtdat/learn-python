742f7d778338be8beca5cf2c8162dc811d227485
Vu Tuan Dat <datvt18@viettel.com.vn>
2020-03-16 11:08:34 +0700

    Add GET method

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,28 @@
 from flask import Flask
+import os
 import json
 
 app = Flask(__name__)
 app.url_map.strict_slashes=True
 
+
 @app.route('/json/')
 def index():
   return json.dumps(os.listdir(os.path.join(app.root_path, 'data')))
 
+
+@app.route('/json/<string:filename>', methods=['GET'])
+def get(filename):
+  msg = ""
+  path = os.path.join(app.root_path, 'data', filename)
+  try:
+    file = open(path, 'r')
+  except FileNotFoundError as err:
+    print("{0}".format(err))
+    msg = "File not found!"
+  else:
+    try:
+      msg = json.load(file)
+    finally:
+      file.close()
+  return msg
