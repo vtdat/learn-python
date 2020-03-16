b86a42ddb8aaae94d0362cbb01008df50242db60
Vu Tuan Dat <datvt18@viettel.com.vn>
2020-03-16 11:07:31 +0700

    Add default page

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
+app.url_map.strict_slashes=True
 
-@app.route('/')
-def hello():
-  return 'Hello, World!'
+@app.route('/json/')
+def index():
+  return json.dumps(os.listdir(os.path.join(app.root_path, 'data')))
 
